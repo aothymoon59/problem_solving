49851cdf677398adbb5373d4abc4259903ac957d
aothymoon59 <aothymoon59@gmail.com>
2024-04-17 23:23:53 +0600

    Build Array from Permutation

---
+++ buildArray.js
@@ -0,0 +1,10 @@
+const buildArray = (nums) => {
+  let res = [];
+  for (let i = 0; i < nums.length; i++) {
+    console.log(nums[i]);
+    res.push(nums[nums[i]]);
+  }
+  return res;
+};
+
+console.log(buildArray([0, 2, 1, 5, 3, 4]));
