d05befc3748b91aeb15f7decbcf9e2106629ea1e
aothymoon59 <aothymoon59@gmail.com>
2024-05-02 23:12:17 +0600

    [SOLVED] hightChecker

---
+++ heightChecker.js
@@ -0,0 +1,18 @@
+const heightChecker = (heights) => {
+  const sortedArr = [...heights].sort((a, b) => a - b);
+  let count = 0;
+  for (let i = 0; i < heights.length; i++) {
+    if (heights[i] !== sortedArr[i]) {
+      count++;
+    }
+  }
+  console.log(sortedArr);
+  console.log(heights);
+  return count;
+};
+
+console.log(
+  heightChecker([
+    10, 6, 6, 10, 10, 9, 8, 8, 3, 3, 8, 2, 1, 5, 1, 9, 5, 2, 7, 4, 7, 7,
+  ])
+);
