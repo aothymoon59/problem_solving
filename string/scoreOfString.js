6f2b7cbfa702493d970fc21a9b93e8b6dbcbd2da
aothymoon59 <aothymoon59@gmail.com>
2024-05-06 23:51:46 +0600

    [SOLVED] scoreOfString other way

--- string/scoreOfString.js
+++ string/scoreOfString.js
@@ -1,10 +1,25 @@
+// Method 1
+// const scoreOfString = (s) => {
+//   let sum = 0;
+//   for (let i = 0; i < s.length - 1; i++) {
+//     if (s.charCodeAt(i) > s.charCodeAt(i + 1)) {
+//       sum += s.charCodeAt(i) - s.charCodeAt(i + 1);
+//     } else {
+//       sum += s.charCodeAt(i + 1) - s.charCodeAt(i);
+//     }
+//   }
+//   return sum;
+// };
+
+// Method 2
 const scoreOfString = (s) => {
   let sum = 0;
   for (let i = 0; i < s.length - 1; i++) {
-    if (s.charCodeAt(i) > s.charCodeAt(i + 1)) {
-      sum += s.charCodeAt(i) - s.charCodeAt(i + 1);
+    let x = s.charCodeAt(i) - s.charCodeAt(i + 1);
+    if (x >= 0) {
+      sum += x;
     } else {
-      sum += s.charCodeAt(i + 1) - s.charCodeAt(i);
+      sum -= x;
     }
   }
   return sum;
