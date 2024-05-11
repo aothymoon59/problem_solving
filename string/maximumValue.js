9fdfd30e5c41177bd75e50e27b8e5b324ec5e9d8
aothymoon59 <aothymoon59@gmail.com>
2024-05-11 23:58:56 +0600

    [SOLVED] maximumValue

--- string/maximumValue.js
+++ string/maximumValue.js
@@ -1,13 +1,13 @@
 const maximumValue = (strs) => {
   let maxVal = 0;
   for (let i = 0; i < strs.length; i++) {
-    if (isNaN(parseInt(strs[i]))) {
+    if (isNaN(Number(strs[i]))) {
       maxVal = Math.max(strs[i].length, maxVal);
     } else {
-      maxVal = Math.max(parseInt(strs[i]), maxVal);
+      maxVal = Math.max(Number(strs[i]), maxVal);
     }
   }
   return maxVal;
 };
 
-console.log(maximumValue(["alic3", "bob", "3", "4", "00000"]));
+console.log(maximumValue(["3glko", "1"]));
